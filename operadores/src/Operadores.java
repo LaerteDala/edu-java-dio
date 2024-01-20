f4ba61d321f8c1d53a4db642069a16085a027447
LaerteDala <laertedala26@gmail.com>
2024-01-20 12:45:34 +0100

    commit de operadores

---
+++ operadores/src/Operadores.java
@@ -0,0 +1,25 @@
+public class Operadores {
+    /**
+     * @param args
+     * @throws Exception
+     */
+          
+        //Opersdores de atribuição
+
+        String nome = "Laerte"; // atribuição de uma variave
+
+        //Operadores aritiméticos
+
+        int num1;
+        int num2;
+
+        int soma = num1 + num2; // Soma
+        int subt = num1 - num2; //subtraçãoo  
+        int div = num1 / num2; // Divisão
+        int modDiv = num1 % num2; // Modulo da divisão 
+
+
+
+
+
+}
