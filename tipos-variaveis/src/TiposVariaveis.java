b14a67f1397421786426e258802f06044d68084a
LaerteDala <laertedala26@gmail.com>
2024-01-20 12:36:39 +0100

    Descrição dos tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        String nome = "Isto é uma cadeia de caracteres";
+        int interiro = 89; // isto é um numero interio
+        long longo = 38783878L; //Isto é um tipo longo 
+        float num_float = 1.5f; // isto é um número de ponto flutuante
+        double num_double = 3767.37; // Isto é um número de ponto flutuante
+        //char genero = "n"; // Isto é um char
+        boolean trueOrFalse = true; // Isto +e um tipo booleanos
+
+        /*Estes são os tipos mais utilizados em Java */
+
+
     }
 }
